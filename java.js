b15cc46cae75a2efbdb43a1d76852a2f6ad246ac
Mikey J <mikeylashaun@icloud.com>
2021-05-12 21:19:52 -0700

    working on js

--- java.js
+++ java.js
@@ -1,10 +1,13 @@
 var wins = 0; 
 var losses = 0; 
 var ties = 0; 
-var myChoice = prompt("Enter rock, paper, or scissors.");
-var computerChoice = ["rock", "paper", "scissors"];
+var myChoice = prompt("Enter r, p, or s.");
+var computerChoice = ["r", "p", "s"];
 
+// random choice of computer
 var randomIndex = Math.floor(Math.random()*3);
-
 var randomChoice = computerChoice[randomIndex];
 
+if(myChoice === "r" && randomChoice === "p"){
+    losses++; 
+}
